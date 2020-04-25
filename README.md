a70d3a0f411a68823ca7e396d219f7e8954ae588
ant <torntrousers@gmail.com>
2020-04-25 17:26:56 +0100

    WIP

--- README.md
+++ README.md
@@ -1,10 +1,28 @@
-# airquality
+# Air Quality
 This is going to document the progress on building an air quailty monitoring station for detecting mainly PM2.5 polution.
-
 
 Data Chart: https://torntrousers.s3.eu-west-2.amazonaws.com/iot/aq.html
 
+Offical London real time air quailty index
+https://aqicn.org/city/london/
 
-Sensors
+Sensors used:
 Common cheap ones are PMS5003/PMS7003 and sds011 etc.
 Some discussion: https://forum.mysensors.org/topic/9317/which-dust-sensor-do-you-use-and-why/16
+
+Docs:
+
+Polluted air carries Corona virus
+https://amp.theguardian.com/environment/2020/apr/24/coronavirus-detected-particles-air-pollution
+
+Air pollution emitted from charcoal grill restaurants
+http://www.pjoes.com/Charcoal-Grill-Restaurants-Deteriorate-Outdoor-r-nAir-Quality-by-Emitting-Volatile,88916,0,2.html
+
+Charcoal grills for cooking meat and fish are not permitted inside restaurants as the government has not included charcoal grills in its list of exempt appliances (seems maybe out of date?)
+https://www.rbkc.gov.uk/printpdf/231
+
+List of authorised fuels for smoke control areas
+https://smokecontrol.defra.gov.uk/fuels.php?country=england
+
+Field evaluation of low-cost particulate matter sensors in high- and low-concentration environments
+https://www.atmos-meas-tech.net/11/4823/2018/amt-11-4823-2018.pdf
--- src/airquality/airquality.ino
+++ src/airquality/airquality.ino
@@ -1,23 +1,24 @@
 /*
  * An Air Quality Monitoring IoT Device
- * 
- * Monitors: 
+ *
+ * Monitors:
  *  - PM1 and PM2.5 particle concentration with a PMS7003 sensor
- *  - temperature and humidity with an SHT31. 
+ *  - temperature and humidity with an SHT31.
  * Runs on an ESP8266 MCU for Wifi connectivity
- * Publishes to Thingspeak for convenient and free storage 
- *  
+ * Publishes to Thingspeak for convenient and free storage
+ *
  * Libraries used:
  * PMS - https://github.com/fu-hsi/pms - v1.1.0
  * SHT3x - Adafruit's SHT3x - v1.1.7
- *  
- * Author: (torntrousers) Anthony Elder 
+ *
+ * Author: (torntrousers) Anthony Elder
  * License: MIT
  */
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
 #include "PMS.h"
 #include "Adafruit_SHT31.h"
+#include "SoftwareSerial.h"
 
 // **** configure these values with your steup and credentials ***
 const char* thingSpeakKey = "N3MQ9PDUQY0RRAT9";
@@ -31,34 +32,40 @@ const char* password = "u3CA3bteMr4t";
 #define sampleIntervalMs  3000
 
 //int tempSamples[publishIntervalMins * 60 / (sampleIntervalMs / 1000)];
-int tempSamples[20];
+float tempSamples[20];
+float humSamples[20];
+uint16_t pm1Samples[20];
+uint16_t pm25Samples[20];
+uint16_t pm10Samples[20];
 
+SoftwareSerial pmsSerial(D4, -1);
 
-PMS pms(Serial);
+PMS pms(pmsSerial);
 PMS::DATA data;
 
 Adafruit_SHT31 sht31 = Adafruit_SHT31();
 
-String temp; 
-String humidity = "3"; 
-String pm1 = "5"; 
-String pm25 = "9.5"; 
+String temp;
+String humidity = "3";
+String pm1 = "5";
+String pm25 = "9.5";
+String pm10 = "9.5";
 
 
 void setup() {
   Serial.begin(115200); Serial.println();
-  Serial.println("setup - airqaulity");  
-  delay(5000);
-  Serial.println("setup - airqaulity2");  
+  Serial.println("setup - airqaulity");
+
   initWifi();
 
-//  Serial.println("Init SHT3x");
-//  if (! sht31.begin(0x44)) {   // Set to 0x45 for alternate i2c addr
-//    Serial.println("Couldn't find SHT31");
-////    while (1) delay(1);
-//  }
+  Serial.println("Init SHT3x");
+  if (! sht31.begin(0x44)) {   // Set to 0x45 for alternate i2c addr
+    Serial.println("Couldn't find SHT31");
+//    while (1) delay(1);
+  }
 
-  
+  pmsSerial.begin(9600, SWSERIAL_8N1, D4, -1, false, 128, 11);
+
 }
 
 long lastPublish;
@@ -73,34 +80,76 @@ void loop() {
   }
 
   if (millis()-lastPublish > publishIntervalMins*60*1000) {
-    avgSamples();
-    sendReading(); 
+    if (avgSamples()) {
+       sendReading();
+    }
     lastPublish = millis();
   }
-  
+
+  delay(2000);
 }
 
-void avgSamples() {
-  int t = 0;
+bool avgSamples() {
+  Serial.println("Averaging readings");
+  if (samples == 0) return false;
+  float ta = 0;
+  float ha = 0;
+  int pm1a = 0;
+  int pm25a = 0;
+  int pm10a = 0;
   for (int i=0; i<samples; i++) {
-    t += tempSamples[i];
-    Serial.print(tempSamples[i]); Serial.println(" ");
+    ta += tempSamples[i];
+    ha += humSamples[i];
+    pm1a += pm1Samples[i];
+    pm25a += pm25Samples[i];
+    pm10a += pm10Samples[i];
   }
-  temp = String((float)t / samples, 1);
+
+  temp = String(ta / samples, 1);
+  humidity = String(ha / samples, 0);
+  pm1 = String(pm1a / samples);
+  pm25 = String(pm25a / samples);
+  pm10 = String(pm10a / samples);
+
   Serial.print("Avg temp: "); Serial.println(temp) ;
+  Serial.print("Avg humidity: "); Serial.println(humidity) ;
+  Serial.print("Avg PM1: "); Serial.println(pm1) ;
+  Serial.print("Avg PM2.5: "); Serial.println(pm25) ;
+  Serial.print("Avg PM10: "); Serial.println(pm10) ;
+ 
   samples = 0;
+  return true;
 }
 
 void takeSample() {
-  tempSamples[samples++] = samples;
+  Serial.println("Taking reading");
+  if (pms.readUntil(data, 3000)) {
+    pm1Samples[samples] = data.PM_SP_UG_1_0;
+    pm25Samples[samples] = data.PM_SP_UG_2_5;
+    pm10Samples[samples] = data.PM_SP_UG_10_0;
+  }
+
+    tempSamples[samples] = sht31.readTemperature();
+    humSamples[samples] = sht31.readHumidity();
+
+    Serial.print("temp: "); Serial.println(tempSamples[samples]);
+    Serial.print("humidity: "); Serial.println(humSamples[samples]);
+    Serial.print("PM 1.0 (ug/m3): "); Serial.println(pm1Samples[samples]);
+    Serial.print("PM 2.5 (ug/m3): "); Serial.println(pm25Samples[samples]);
+    Serial.print("PM 10.0 (ug/m3): "); Serial.println(pm10Samples[samples]);
+
+    samples = samples + 1;
+//  }
+
 }
 
 void sendReading() {
   String url = String("http://api.thingspeak.com/update?api_key=") + thingSpeakKey +
                "&field1=" + temp +
-               "&field2=" + humidity + 
-               "&field3=" + pm1 + 
-               "&field4=" + pm25; 
+               "&field2=" + humidity +
+               "&field3=" + pm1 +
+               "&field4=" + pm25 +
+               "&field5=" + pm10;
 
   Serial.print("Publishing: "); Serial.println(url);
   HTTPClient http;
@@ -116,5 +165,5 @@ void initWifi() {
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
   WiFi.waitForConnectResult();
-  Serial.print(", connected. IP: "); Serial.println(WiFi.localIP());
+  Serial.print("connected, IP: "); Serial.println(WiFi.localIP());
 }
